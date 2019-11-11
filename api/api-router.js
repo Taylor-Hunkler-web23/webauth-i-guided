3e04ce46fe01f1b902bc4e81aa071e33d8512870
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-11 12:35:37 -0500

    Creating hash password

--- api/api-router.js
+++ api/api-router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const bcrypt = require ('bcryptjs')
 
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
@@ -9,5 +10,24 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+
+const hash = bcrypt.hashSync(user.password, 12)
+
+user.password=hash;
+  let user = req.body
+
+rourter.post('/hash', (req, res) => {
+  // read a password from the body
+  // hash the password using bcryptjs
+  // return it to the user in an object that looks like
+  // { password: 'original passsword', hash: 'hashed password' }
+
+  .then(saved => {
+    res.satus(201).json(saved)
+
+  })
+})
+
 
 module.exports = router;
